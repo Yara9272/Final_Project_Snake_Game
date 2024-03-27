8493888b6771537c1c8c15fd0966c7a25b7714c7
Yara <yarakhochkhoch2001@gmail.com>
2024-03-27 17:08:05 +0200

    Adjust snake appearance.

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,142 @@
+// Flutter material library for UI components
 import 'package:flutter/material.dart';
+// dart math library for generating random numbers
 import 'dart:math';
+// dart async library for asynchronous programming
 import 'dart:async';
 
 
 void main() {
-  runApp(const SnakeGame());
+  runApp(const SnakeGame()); // Entry point of the application, starting the SnakeGame widget
 }
 
-class SnakeGame extends StatelessWidget {
-  const SnakeGame({super.key});
 
-  // This widget is the root of your application.
+
+class SnakeGame extends StatelessWidget {
+  const SnakeGame({super.key}); // Constructor for the SnakeGame widget
+
   @override
   Widget build(BuildContext context) {
+    // Build method for the SnakeGame widget, returning the MaterialApp widget
     return MaterialApp(
       title: 'Snake Game',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const MySnakeGame(),
+      home: const MySnakeGame(), // Setting the home screen to MySnakeGame widget
     );
   }
 }
 
+
+
 class MySnakeGame extends StatefulWidget {
-  const MySnakeGame({Key? key}): super(key: key);
+  const MySnakeGame({Key? key}): super(key: key); // Constructor for the MySnakeGame widget
   @override
-  State<MySnakeGame> createState() => _MySnakeGameState();
+  State<MySnakeGame> createState() => _MySnakeGameState(); // Creating the state for MySnakeGame
 }
 
 
-enum Direction { up, down, left, right }
+
+enum Direction { up, down, left, right } // Directions the snake can take
+
 // Create a class to hold the calculated values
 class GameConfig {
-  static int crossAxisCount=1;
-  static int rowCount=1;
+  static int crossAxisCount = 1; // Static variable to hold the count of columns
+  static int rowCount = 1; // Static variable to hold the count of rows
 }
+
 
 
 class _MySnakeGameState extends State<MySnakeGame> {
-  List<int> snakePosition = [300, 301, 302]; //intial snake position
-  int foodLocation = Random().nextInt(GameConfig.crossAxisCount*GameConfig.rowCount); //initial food location, chosen randomly
-
-  Direction direction = Direction.right;
-
-
+  List<int> snakePosition = [300, 301, 302]; // Initial position of the snake
+  int foodLocation = Random().nextInt(GameConfig.crossAxisCount * GameConfig.rowCount); // Initial food location, chosen randomly
+  Direction direction = Direction.right; // Initial direction of the snake
 
 
   @override
-  void initState(){
-    Timer.periodic(Duration(milliseconds: 200),
-        onGameTick);
+  void initState() {
+    // Initialize method called when the state object is first created
+    Timer.periodic(Duration(milliseconds: 200), onGameTick); // Start a timer to call onGameTick method periodically
   }
 
 
   void onGameTick(Timer timer) {
-    updateSnake(); //handle the direction of the snake based on the current direction
+    // Method called on each tick of the timer
+    updateSnake(); // Update the position of the snake
   }
 
 
   @override
   Widget build(BuildContext context) {
     // Calculate the number of squares needed in each dimension
-    final double screenWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
-    final double screenHeight = MediaQuery
-        .of(context)
-        .size
-        .height;
-    GameConfig.crossAxisCount = (screenWidth / 12).ceil();
-    GameConfig.rowCount = (screenHeight / 12).ceil();
-    final int itemCount = GameConfig.crossAxisCount * GameConfig.rowCount;
-
-    print( GameConfig.crossAxisCount);
-    print(GameConfig.rowCount);
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    GameConfig.crossAxisCount = (screenWidth / 12).ceil(); // Number of squares in the horizontal direction
+    GameConfig.rowCount = (screenHeight / 12).ceil(); // Number of squares in the vertical direction
+    final int itemCount = GameConfig.crossAxisCount * GameConfig.rowCount; // Total number of squares on the screen
 
     // Determine the indices of the tip and tail of the snake
-    int tipIndex = snakePosition.last;
-    int tailIndex = snakePosition.first;
+    int tipIndex = snakePosition.last; // Index of the tip of the snake
+    int tailIndex = snakePosition.first; // Index of the tail of the snake
 
     return Scaffold(
       body: SafeArea(
         child: GestureDetector(
+          // Gesture detector to handle drag events
           onVerticalDragUpdate: (details) {
+            // Update direction based on vertical drag movement
             if (direction != Direction.up && details.delta.dy > 0) {
-              direction = Direction.down;
+              direction = Direction.down; // Set direction to down if not already moving up
             }
             if (direction != Direction.down && details.delta.dy < 0) {
-              direction = Direction.up;
+              direction = Direction.up; // Set direction to up if not already moving down
             }
           },
           onHorizontalDragUpdate: (details) {
+            // Update direction based on horizontal drag movement
             if (direction != Direction.left && details.delta.dx > 0) {
-              direction = Direction.right;
+              direction = Direction.right; // Set direction to right if not already moving left
             }
             if (direction != Direction.right && details.delta.dx < 0) {
-              direction = Direction.left;
+              direction = Direction.left; // Set direction to left if not already moving right
             }
           },
+
           child: Container(
             color: Colors.grey[900], // Background color of the container
+
             child: GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
+              physics: const NeverScrollableScrollPhysics(), // Disable scrolling in the GridView
               itemCount: itemCount,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: GameConfig.crossAxisCount,
-                childAspectRatio: screenWidth / GameConfig.crossAxisCount / screenHeight *
-                    GameConfig.rowCount,
+                crossAxisCount: GameConfig.crossAxisCount, // Number of columns in the GridView
+                childAspectRatio: screenWidth / GameConfig.crossAxisCount / screenHeight * GameConfig.rowCount,
+                // Aspect ratio of each grid item
               ),
+
               itemBuilder: (context, index) {
+                // Builder function for creating grid items
                 if (snakePosition.contains(index)) {
-                  // Check if the current index is the tip or tail of the snake
-                  if (index == tipIndex){
+                  // Check if the snake occupies the current grid cell
+                  if (index == tipIndex) {
+                    // Check if the snake's head is at this index
                     return Container(
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(9.0), // Round only top right corner
-                          bottomRight: Radius.circular(9.0), // Round only bottom right corner
-                        ),
-                        color: Colors.green[900],
+                        borderRadius: _getTipBorderRadius(), // Apply border radius to snake head
+                        color: Colors.green[900], // Color of the snake's head with a darker shade of green
                       ),
                     );
-                  } else if (index == tailIndex){
+                  } else if (index == tailIndex) {
+                    // Check if the snake's tail is at this index
                     return Container(
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(9.0), // Round only top right corner
-                          bottomLeft: Radius.circular(9.0), // Round only bottom right corner
-                        ),
+                        borderRadius: _getTailBorderRadius(), // Apply border radius to snake tail
                         color: Colors.green,
                       ),
                     );
-                  }else{
+                  } else {
+                    // Otherwise, render a regular segment of the snake body
                     return Container(
                       decoration: BoxDecoration(
                         shape: BoxShape.rectangle,
@@ -142,18 +145,23 @@ class _MySnakeGameState extends State<MySnakeGame> {
                     );
                   }
                 }
+
                 if (index == foodLocation) {
+                  // Check if the current grid cell contains food
                   return Container(
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       color: Colors.red,
-                    ),                );
+                    ),
+                  );
                 }
+                // Render empty grid cells
                 return Container(
                   decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.grey[900],
-                  ),              );
+                    shape: BoxShape.circle, // Shape of the container
+                    color: Colors.grey[900], // Background color of the container
+                  ),
+                );
               },
             ),
           ),
@@ -163,25 +171,92 @@ class _MySnakeGameState extends State<MySnakeGame> {
   }
 
 
+  // Function to determine border radius for the tip based on direction
+  BorderRadius _getTipBorderRadius() {
+    switch (direction) {
+      case Direction.up:
+      // If snake is moving up, give rounded corners to the top
+        return BorderRadius.only(
+          topLeft: Radius.circular(9.0),
+          topRight: Radius.circular(9.0),
+        );
+      case Direction.down:
+      // If snake is moving down, give rounded corners to the bottom
+        return BorderRadius.only(
+          bottomLeft: Radius.circular(9.0),
+          bottomRight: Radius.circular(9.0),
+        );
+      case Direction.left:
+      // If snake is moving left, give rounded corners to the left
+        return BorderRadius.only(
+          topLeft: Radius.circular(9.0),
+          bottomLeft: Radius.circular(9.0),
+        );
+      case Direction.right:
+      // If snake is moving right, give rounded corners to the right
+        return BorderRadius.only(
+          topRight: Radius.circular(9.0),
+          bottomRight: Radius.circular(9.0),
+        );
+      default:
+      // If direction is not recognized, return zero radius
+        return BorderRadius.zero;
+    }
+  }
+
+
+// Function to determine border radius for the tail based on direction
+  BorderRadius _getTailBorderRadius() {
+    switch (direction) {
+      case Direction.up:
+      // If snake is moving up, give rounded corners to the bottom
+        return BorderRadius.only(
+          bottomLeft: Radius.circular(9.0),
+          bottomRight: Radius.circular(9.0),
+        );
+      case Direction.down:
+      // If snake is down up, give rounded corners to the top
+        return BorderRadius.only(
+          topLeft: Radius.circular(9.0),
+          topRight: Radius.circular(9.0),
+        );
+      case Direction.left:
+      // If snake is moving left, give rounded corners to the right
+        return BorderRadius.only(
+          topRight: Radius.circular(9.0),
+          bottomRight: Radius.circular(9.0),
+        );
+      case Direction.right:
+      // If snake is moving right, give rounded corners to the left
+        return BorderRadius.only(
+          topLeft: Radius.circular(9.0),
+          bottomLeft: Radius.circular(9.0),
+        );
+      default:
+        return BorderRadius.zero;
+    }
+  }
+
+
   void updateSnake() {
     setState(() {
       // Determine the current position of the snake's head
-      int head = snakePosition.last;
-      int nextCell = 0;
+      int head = snakePosition.last; // Index of the snake's head
 
       // Calculate the next position based on the direction
+      int nextCell = 0; // Initialize variable to hold the next cell's index
       switch (direction) {
         case Direction.up:
-          nextCell = head - GameConfig.crossAxisCount;
+          nextCell = head - GameConfig.crossAxisCount; // Move one row up
           break;
         case Direction.down:
-          nextCell = head + GameConfig.crossAxisCount;
+          nextCell = head + GameConfig.crossAxisCount; // Move one row down
           break;
         case Direction.left:
-          nextCell = head - 1;
+          nextCell = head - 1; // Move one column to the left
           break;
         case Direction.right:
-          nextCell = head + 1;
+          nextCell = head + 1; // Move one column to the right
           break;
       }
 
@@ -193,21 +268,26 @@ class _MySnakeGameState extends State<MySnakeGame> {
         // Generate a new random location for the food
         foodLocation = Random().nextInt(GameConfig.crossAxisCount * GameConfig.rowCount);
       } else {
+        // If the snake doesn't eat the food
         // Remove the tail segment of the snake
         snakePosition.removeAt(0);
 
         // Check for collision with boundaries
         if (nextCell < 0 ||
-            nextCell >= GameConfig.crossAxisCount*GameConfig.rowCount || // Assuming a 100x30 grid
-            (direction == Direction.left && head % GameConfig.crossAxisCount == 0) || // Hit left wall
-            (direction == Direction.right && (head + 1) % GameConfig.crossAxisCount == 0)) {
-          // Hit right wall
+            nextCell >= GameConfig.crossAxisCount * GameConfig.rowCount || // Check for out-of-bounds condition
+            (direction == Direction.left && head % GameConfig.crossAxisCount == 0) || // Check for collision with left wall
+            (direction == Direction.right && (head + 1) % GameConfig.crossAxisCount == 0)) { // Check for collision with right wall
           // Game over condition, you may handle it accordingly
           // For now, let's reset the game
+
+          //Reset the game
+          // Reset snake's position
           snakePosition = [300, 301, 302];
+          // Generate new random food location
           foodLocation = Random().nextInt(GameConfig.crossAxisCount * GameConfig.rowCount);
-          direction = Direction.right; // Reset direction
-          return;
+          // Reset direction to right
+          direction = Direction.right;
+          return; // Exit the function to prevent further processing
         }
 
         // Move the snake's head to the next cell
